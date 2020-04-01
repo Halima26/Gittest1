3c80c28ea392d37bd8e87f54b70aaf972b77a483
Halima26 <62764798+Halima26@users.noreply.github.com>
2020-04-01 00:24:00 -0700

    MY first Save

---
+++ src/HelloWorld.java
@@ -0,0 +1,17 @@
+public class HelloWorld {
+    public static void main (String[]args ){
+        int hours = 40;
+        double grossPay;
+        double payRate =25;
+        String Name = "Sanjay";
+
+//  To get grosspay we multiply hours with payrate.
+        grossPay =hours * payRate;
+        System.out.println("My name is " + Name + " My Weekly Salary is $" + grossPay);
+
+
+
+
+    }
+    }
+
---
+++ src/chapter1.java
@@ -0,0 +1,22 @@
+public class chapter1 {
+    public static void main(String[]args){
+        String Name="SaneyAlam ";
+         int Age=30;
+         Double annualPay=100000.0;
+
+
+
+         System.out.println("My Name is SaneyAlam " );
+         System.out.println("I am 30 years old ");
+         System.out.println("I hope to earn $1000000.0 per year ");
+
+
+
+
+
+
+
+
+
+    }
+}
